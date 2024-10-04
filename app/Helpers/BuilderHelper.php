94299bdb072443bf1bf807fee71d9198722b0813
Phiricen Aoba <phiricen+i@gmail.com>
2024-10-05 00:27:18 +0900

    更新

---
+++ app/Helpers/BuilderHelper.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace AobaTw\ExtLaravel\App\Helpers;
+
+class BuilderHelper
+{
+    public static function getRawSQL(Builder $builder)
+    {
+        dd($builder);
+    }
+}
--- app/Helpers/DiscordHelper.php
+++ app/Helpers/DiscordHelper.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Helpers;
+namespace AobaTw\ExtLaravel\App\Helpers;
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
